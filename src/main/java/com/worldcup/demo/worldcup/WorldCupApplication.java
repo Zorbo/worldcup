ac14d6df9861e2982d4f18ff72cbd782c5be8264
tamas.a.kiss <tamas.a.kiss@accenture.com>
2019-08-07 13:54:29 +0200

    - Java FX progress, creating frontend add Tables

--- src/main/java/com/worldcup/demo/worldcup/WorldCupApplication.java
+++ src/main/java/com/worldcup/demo/worldcup/WorldCupApplication.java
@@ -2,7 +2,6 @@ package com.worldcup.demo.worldcup;
 
 import com.worldcup.demo.worldcup.service.Couples;
 import com.worldcup.demo.worldcup.service.Cup;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import javafx.application.Application;
@@ -27,10 +26,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class WorldCupApplication extends Application {
 
-	private final static String DATA = "C:\\Training\\worldcup\\src\\main\\resources\\data.txt";
+	private static final String DATA = "C:\\Training\\worldcup\\src\\main\\resources\\data.txt";
 	private static final Logger logger = LoggerFactory.getLogger(WorldCupApplication.class);
+	private static final String REGEX_SPLIT_COMMA = "\\s*,\\s*";
 	private ConfigurableApplicationContext springContext;
 	private Cup cup;
+	private ObservableList<String> husbandData;
+	private ObservableList<String> wifeData;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -44,24 +46,16 @@ public class WorldCupApplication extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		primaryStage.setTitle("World Cup");
 		// Create resources on startup
+		primaryStage.setTitle("World Cup");
 		Couples couples = new Couples(DATA);
-		Button matchButton = new Button("Match!");
-		// Set the button function
-		matchButton.setOnAction(event -> {
-			 this.cup = new Cup(DATA);
-			watchMatch(couples,cup);
-		});
-		// Set the button element position
-		matchButton.setLayoutX(480);
-		matchButton.setLayoutY(650);
-
+		Pane root = new Pane();
+		Scene scene = new Scene(root, 200, 100);
 
 		// TODO: add 3 TableView, 3 static text, 1 Team1:Team2 text and 2 counting text
 
 		// Create couple view
-		Label coupleViewLabel = new Label("Hazasparok");
+		Label coupleViewLabel = new Label("Couples");
 		ObservableList<String> coupleData = FXCollections.observableArrayList(createCoupleView(couples));
 		TableView<String> coupleView = new TableView<>();
 		TableColumn<String, String> col1 = new TableColumn<>();
@@ -76,15 +70,56 @@ public class WorldCupApplication extends Application {
 		coupleSp.setLayoutY(100);
 
 		// Create Husband view
+		Label husbandLabel = new Label("Husbands");
+		husbandData = FXCollections.observableArrayList(createHusbandView(couples));
+		TableView<String> husbandView = new TableView<>();
+		TableColumn<String, String> husbandColumn = new TableColumn<>();
+		husbandColumn.setMinWidth(100);
+		husbandView.getColumns().add(husbandColumn);
+		husbandColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+		husbandView.setItems(husbandData);
+		StackPane husbandSp = new StackPane(husbandView);
+		husbandSp.getChildren().add(husbandLabel);
+		husbandSp.setAlignment(Pos.TOP_CENTER);
+		husbandSp.setPrefSize(200, 400);
+		husbandSp.setLayoutX(400);
+		husbandSp.setLayoutY(100);
 
 		//Create Wife view
+		Label wifeLabel = new Label("Wives");
+		wifeData = FXCollections.observableArrayList(createWifeView(couples));
+		TableView<String> wifeView = new TableView<>();
+		TableColumn<String, String> wifeColumn = new TableColumn<>();
+		wifeColumn.setMinWidth(100);
+		wifeView.getColumns().add(wifeColumn);
+		wifeColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+		wifeView.setItems(wifeData);
+		StackPane wifeSp = new StackPane(wifeView);
+		wifeSp.getChildren().add(wifeLabel);
+		wifeSp.setAlignment(Pos.TOP_CENTER);
+		wifeSp.setPrefSize(200,400);
+		wifeSp.setLayoutX(750);
+		wifeSp.setLayoutY(100);
 
+		//Create the match button
+		Button matchButton = new Button("Match!");
+		matchButton.setLayoutX(480);
+		matchButton.setLayoutY(650);
+		matchButton.setOnAction(event -> {
+			this.cup = new Cup(DATA);
+			watchMatch(couples,cup);
+			husbandData.clear();
+			husbandData = FXCollections.observableArrayList(createHusbandView(couples));
+			updateHusband(husbandView,husbandColumn);
+			wifeData.clear();
+			wifeData = FXCollections.observableArrayList(createWifeView(couples));
+			updateWife(wifeView,wifeColumn);
 
-
+		});
+		
 		// Init Desktop view
-		Pane root = new Pane();
-		Scene scene = new Scene(root, 200, 100);
-		root.getChildren().addAll(coupleSp,matchButton);
+
+		root.getChildren().addAll(coupleSp, husbandSp, wifeSp ,matchButton);
 		primaryStage.setScene(scene);
 		primaryStage.setWidth(1024);
 		primaryStage.setHeight(768);
@@ -102,12 +137,47 @@ public class WorldCupApplication extends Application {
 																  .append("-")
 																  .append(value.getName())
 																  .append(","));
-		return Arrays.asList(stringBuilder.toString().split("\\s*,\\s*"));
+		return Arrays.asList(stringBuilder.toString().split(REGEX_SPLIT_COMMA));
+	}
+
+	private List<String> createHusbandView(Couples couples) {
+		StringBuilder stringBuilder = new StringBuilder();
+		couples.getCouples().keySet().forEach(k -> stringBuilder.append(k.getName())
+																.append(" - ")
+																.append(k.getBeers())
+																.append(" sort ivott.,"));
+		return Arrays.asList(stringBuilder.toString().split(REGEX_SPLIT_COMMA));
+	}
+
+	private void updateHusband(TableView<String> husbandView, TableColumn<String, String> husbandColumn) {
+		husbandView.getColumns().clear();
+		husbandView.getColumns().add(husbandColumn);
+		husbandColumn.setMinWidth(100);
+		husbandColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+		husbandView.setItems(husbandData);
+		husbandView.refresh();
+	}
+
+	private List<String> createWifeView(Couples couples) {
+		StringBuilder stringBuilder = new StringBuilder();
+		couples.getCouples().values().forEach(v -> stringBuilder.append(v.getName())
+																.append(" - ")
+																.append("szabadideje: ")
+																.append(v.getFreeTime())
+																.append(" perc.,"));
+		return Arrays.asList(stringBuilder.toString().split(REGEX_SPLIT_COMMA));
+	}
+
+	private void updateWife(TableView<String> wifeView, TableColumn<String, String> wifeColumn) {
+		wifeView.getColumns().clear();
+		wifeView.getColumns().add(wifeColumn);
+		wifeColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+		wifeView.setItems(wifeData);
+		wifeView.refresh();
 	}
 
 	@Override
 	public void stop() {
 		springContext.stop();
 	}
-
 }
